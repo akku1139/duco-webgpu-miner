79b267bb2893dc97b240528e2b10d2a5e92d75d4
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-06 19:47:51 +0900

    wip

--- src/miner/gpu/webgpu.ts
+++ src/miner/gpu/webgpu.ts
@@ -3,6 +3,8 @@ import { PoolManager, type Job } from "../pool.ts"
 import { WorkerLog } from "../workerLog.ts"
 import type { Config } from "@/lib/types.ts"
 
+import ducos1Shader from "./shaders/ducos1.wgsl?raw"
+
 let pool: PoolManager
 
 let log: WorkerLog
@@ -22,6 +24,32 @@ addEventListener("message", async (e) => {
 })
 
 const start = async () => {
+  // https://webgpufundamentals.org/webgpu/lessons/ja/webgpu-fundamentals.html
+  if (!navigator.gpu) {
+    log.emit(mod, text.color("disabled. this browser does not support WebGPU", "red"))
+    return
+  }
+
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    log.emit(mod, text.color("disabled. this browser supports webgpu but it appears disabled", "red"))
+    return
+  }
+
+  const device = await adapter?.requestDevice()
+  device.lost.then((info) => {
+    log.emit(mod, `WebGPU device was lost: ${info.message} (${info.reason})`)
+
+    // 'reason' will be 'destroyed' if we intentionally destroy the device.
+    //if (info.reason !== "destroyed") {
+      // try again
+    //}
+  })
+
+  device.createShaderModule({
+    code: ducos1Shader,
+  })
+
   let job: Job
   let hashHex: string = ""
 
